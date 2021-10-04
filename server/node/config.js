93dd9e632f958105374ad32bd107bb6b9d3cc94f
Gaurav Pandey <gaurav.golu1@gmail.com>
2021-10-04 21:30:28 +0530

    adding .env to gitignore

--- server/node/config.js
+++ server/node/config.js
@@ -10,9 +10,9 @@ require('dotenv').config();
 module.exports = {
   // Server port.
   port: process.env.PORT || 5000,
-  api_url: process.env.API_URL,
-  anumati_url: process.env.ANUMATI_URL,
+  api_url: process.env.SETU_API_URL,
+  anumati_url: process.env.SETU_ANUMATI_URL,
   client_api_key: process.env.CLIENT_API_KEY,
-  rahasya_url: process.env.RAHASYA_URL,
+  rahasya_url: process.env.SETU_RAHASYA_URL,
   your_client_url: process.env.YOUR_CLIENT_URL,
 };
